74de0abe15e9dee56fbd80b5c87a1e15253913a2
SnowFleur <SnowFleur0128@gmail.com>
2021-10-06 22:27:19 +0900

    [SnowFleur] LogCollector.h, NetAddress Update

--- Basic-SnowNetCore/NetAddress.cpp
+++ Basic-SnowNetCore/NetAddress.cpp
@@ -1,26 +1,41 @@
-#include"NetAddress.h"
+#include "NetAddress.h"
 #include"LogCollector.h"
 
-CNetAddress::CNetAddress(){
+CNetAddress::CNetAddress()
+{
     Init();
 }
 
-void CNetAddress::Init(){
-    ZeroMemory(&acceptClientInfor_, sizeof(acceptClientInfor_));
+void CNetAddress::Init()
+{
+    ZeroMemory(&stSockAddrInfor_, sizeof(stSockAddrInfor_));
 }
 
-void CNetAddress::SetAddrInfor(const PSOCKADDR addrinfor) {
-    memcpy_s(&acceptClientInfor_, sizeof(SOCKADDR_IN), addrinfor, sizeof(SOCKADDR_IN));
+void CNetAddress::SetAddrInfor(const PSOCKADDR addrinfor)
+{
+    memcpy_s(&stSockAddrInfor_, sizeof(SOCKADDR_IN), addrinfor, sizeof(SOCKADDR_IN));
 }
 
-void CNetAddress::PrintIPAndPort()const{
+//Only Ipv4
+void CNetAddress::SetAddrInfor(const char* ip, const USHORT port)
+{
+    if (ip == nullptr)return;
+
+    stSockAddrInfor_.sin_family = AF_INET;
+    stSockAddrInfor_.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &stSockAddrInfor_.sin_addr);
+}
+
+void CNetAddress::PrintIPAndPort()const
+{
     char buf[32]{ 0 };      //4Byte
-    inet_ntop(AF_INET, &acceptClientInfor_.sin_addr, buf, sizeof(buf));
-    PRINT_INFO_LOG("Accpet IP:", buf,"PORT:",ntohs(acceptClientInfor_.sin_port));
+    inet_ntop(AF_INET, &stSockAddrInfor_.sin_addr, buf, sizeof(buf));
+    PRINT_INFO_LOG("IP:", buf, "PORT:", ntohs(stSockAddrInfor_.sin_port));
 }
 
-const PSOCKADDR_IN CNetAddress::GetAddrInfor() {
-    return &acceptClientInfor_;
+const PSOCKADDR_IN CNetAddress::GetAddrInfor()
+{
+    return &stSockAddrInfor_;
 }
 
 
